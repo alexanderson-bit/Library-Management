f458f5058f0621615dcac2b789fcafc1e916ed34
alex <alex.anderson.office@gmail.com>
2026-08-02 17:36:23 +0330

    add set for usert attribute

---
+++ module/user.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self,national_code,fname,lname,city,dad_name,password):
+        pass
+    def set_name(self,fname,lname):
+        self.fname=fname
+        self.lname=lname
+    def set_national_code(self,national_code):
+        self.national_code=national_code
+    def set_city(self,city):
+        self.city=city
+    def set_dad_name(self,dad_name):
+        self.dad_name=dad_name
+    def set_password(self,password):
+        self.password=password    
+         

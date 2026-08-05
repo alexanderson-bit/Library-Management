f8022d4463b29c268c05029dd2ed31cc3870c966
alex <alex.anderson.office@gmail.com>
2026-08-05 13:17:06 +0330

    add method for checking and read  strong password

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import string
 # import pickle
 # class name:
 #     def __init__(self,name,age):
@@ -19,18 +20,42 @@
 def read_national_code():
     while True:
         national_code=input("Enter National ID (10 digits) :")
+        if len(national_code)!=10:
+            print("The number of digits must be ten.")
+            continue
         for i in national_code:
             if "0"<=i<="9":
                 pass
             else:
                 print("Only numbers should be entered.")
                 break
+
+        return national_code    
+
+def checking_strong_password():
+    while True:
+        sum=0
+        password=input("Enter password :")
+        if len(password)<8:
+            print("The password must be longer than 8 characters.")
+            continue
+        for c in password:
+            if c==" ":
+                continue
+        if any(c.isupper() for c in password):
+            sum+=1
+        if any(c.islower() for c in password):
+            sum+=1
+        if any(c.isdigit() for c in password):
+            sum+=1
+        if  any( c in string.punctuation for c in password):
+            sum+=1   
+        if sum==4:
+            return password
         else:
-            if len(national_code)==10:
-                return national_code    
-            else:
-                print("The number of digits must be ten.")
+            print("Password must contain at least an uppercase letter, lowercase letter, special character and at least one digit")    
 
 
-m=read_national_code()
-print(m)            
+
+m=checking_strong_password()
+print(m)
